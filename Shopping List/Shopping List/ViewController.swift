73ecee97c28c1633908874608e022d2aa72f6e13
akshayrameshappdev <akshay_ramesh@yahoo.com>
2020-12-29 21:03:18 -0600

    Finished the shopping list project

--- Shopping List/Shopping List/ViewController.swift
+++ Shopping List/Shopping List/ViewController.swift
@@ -12,8 +12,36 @@ class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        shoppingList.append("Buy Milk")
-        shoppingList.append("But Drink")
+        initNavBar()
+    }
+    
+    func initNavBar() {
+        title = "Shopping List"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addToShoppingList))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(clearShoppingList))
+    }
+    
+    @objc func clearShoppingList() {
+        shoppingList.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+    @objc func addToShoppingList() {
+        let itemController = UIAlertController(title: "Add item", message: "Please enter any item name from this shop and click add to list", preferredStyle: .alert)
+        itemController.addTextField()
+        let addAction = UIAlertAction(title: "Add To List", style: .default) { [weak self, weak itemController] _ in
+            guard let item = itemController?.textFields?[0].text else { return }
+            self?.submit(item)
+        }
+        itemController.addAction(addAction)
+        present(itemController, animated: true)
+    }
+    
+    func submit(_ item: String) {
+        shoppingList.insert(item, at: 0)
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+        return
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
